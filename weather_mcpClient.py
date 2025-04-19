afc0ba8a50e4511531bb12b618b290595cc10087
Jatin Mehra <dev.jatin.singh@gmail.com>
2025-04-19 10:16:17 +0530

    basic-clientMCP

--- weather_mcpClient.py
+++ weather_mcpClient.py
@@ -1,15 +1,37 @@
 import requests 
 import json 
 
-MCP_SERVER_URL = "https://localhost:8000"
+MCP_SERVER_URL = "http://localhost:8000"
 
 def get_current_location():
-    return "Sukha , Uttarakhand"
+    return input("Enter a location")
 
 def ask_for_weather(location:str):
-    print("Will Do !")
+    payload = {
+        "method" : "get_weather",
+        "params" : {"location":location},
+    }
+    headers = {"Content-Type":"application/json"}
+    try:
+        response = requests.post(MCP_SERVER_URL,headers=headers,json=payload)
+        response.raise_for_status()
+        data = response.json()
+        if "result" in data:
+            weather_data = data["result"]
+            print(f"Current Weather in the location {location} is :")
+            print(f"Temperature : {weather_data.get('temperature')} Celsius")
+            print(f"Conditions : {weather_data.get('conditions')}")
+            print(f"Humidity : {weather_data.get('humidity')}%")
+            print(f"Wind Speed : {weather_data.get('wind_speed')}km/h")
+        elif "error" in data:
+            print(f"Error : {data['error']}")
+        else:
+            print(f"Unexpected Response : {data}")
 
-if __name__=='__main':
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to MCP Server : {e}")
+
+if __name__=='__main__':
     curr_location = get_current_location()
     print(f"My current location is : {curr_location}")
-    ask_for_weather()
+    ask_for_weather(curr_location)
